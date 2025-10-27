13463fedad5a4ae165f3b92c7b5e35d66a93bd43
Tom Esparon <tespo@btinternet.com>
2025-10-27 11:47:31 +0000

    edit

---
+++ .bash_installers/msedit_install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Install script for ubuntu based non snap
+# Termux is just pkg install msedit
+ARCH=$(uname -m)
+RELEASE_VERSION="v1.2.1"
+BINARY_VERSION="1.2.0"
+
+if  [[ "${ARCH}" == "x86_64" ]]; then
+    wget "https://github.com/microsoft/edit/releases/download/${RELEASE_VERSION}/edit-${BINARY_VERSION}-${ARCH}-linux-gnu.tar.zst"
+elif [[ "${ARCH}" == "aarch64" ]]; then
+    wget "https://github.com/microsoft/edit/releases/download/${RELEASE_VERSION}/edit-${BINARY_VERSION}-${ARCH}-linux-gnu.tar.zst"
+else
+    echo "No supported arch build found"
+fi
+
+tar --zstd -xvf edit-${BINARY_VERSION}-${ARCH}-linux-gnu.tar.zst
+cp edit ~/.local/bin/
+exec bash
+
